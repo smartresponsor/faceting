5a9f90ff5bf7dadce32abbf5ef1022995ff0a558
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-04-01 20:24:22 -0500

    build(faceting): add config bootstrap

---
+++ config/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (class_exists(Dotenv::class)) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
