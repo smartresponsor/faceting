b46e3f467f08624a94e381d3dd3ecd8a4419bf23
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-04-01 20:41:29 -0500

    build(faceting): add public index entrypoint

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'/config/bootstrap.php';
+
+if (($_SERVER['APP_DEBUG'] ?? '0') === '1') {
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool) ($_SERVER['APP_DEBUG'] ?? true));
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
